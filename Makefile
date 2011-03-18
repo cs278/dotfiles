cs278/dotfiles

commit 0e35fd4b9729ef5ecb504ec95ea67608e5a37b9b

Don't install the README file.

Makefile
@@ -33,6 +33,7 @@ install:
 	for file in *; do \
 		case "$$file" in \
 			"Makefile") \
+			"README*") \
 				echo "Skipping $$file"; \
 			;; \
 			*) \

commit 5bd171c76164699d8d31a36ad379a1158c6ae793

Fix bug skipping over README file introduced in 0e35fd4.

Makefile
@@ -32,8 +32,7 @@ install:
 	cd $(SRC) && \
 	for file in *; do \
 		case "$$file" in \
-			"Makefile") \
-			"README*") \
+			Makefile|README*) \
 				echo "Skipping $$file"; \
 			;; \
 			*) \