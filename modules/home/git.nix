900cdd58bf47496da52d71b9479bce995c346ae6
Chris Smith <chris@cs278.org>
2024-08-12 23:47:06 +0100

    wip

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,8 +1,44 @@
-programs.git = {
-  enable = true;
-  delta.enable = true;
+{
+  inputs,
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  programs.git = {
+    enable = true;
+    delta.enable = true;
 
-  userName = "Chris Smith";
-};
+    userName = "Chris Smith";
 
-programs.gh.enable = true;
+    attributes = [
+      "*.png diff=image"
+      "*.jpg diff=image"
+      "*.jpeg diff=image"
+      "*.gif diff=image"
+      "*.json diff=json"
+    ];
+
+    ignores = [
+      # Backups
+      "*~"
+      # Editor configuration
+      ".idea"
+      ".vscode"
+      # OS generated junk
+      ".DS_Store"
+      "Desktop.ini"
+      "Thumbs.db"
+    ];
+
+    includes = [
+      { path = "~/.config/git/extra"; }
+    ];
+  };
+
+  home.file."${config.xdg.configHome}/git/extra" = {
+    source = ../../files/git/config;
+  };
+
+  programs.gh.enable = true;
+}
