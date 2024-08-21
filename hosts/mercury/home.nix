a52b7c4e74e33073c0c2711b81c56939575c8fd2
Chris Smith <chris@cs278.org>
2024-08-21 17:06:26 +0100

    [WIP] -m \'So I hear you like commits ...\'

--- hosts/mercury/home.nix
+++ hosts/mercury/home.nix
@@ -92,8 +92,11 @@
   #
   home.sessionVariables = {
     EMAIL = "chris@cs278.org";
-    SSH_AUTH_SOCK = "/run/user/1000/yubikey-agent/yubikey-agent.sock"; # todo remove hard coded 1000?
     # EDITOR = "emacs";
+  };
+
+  programs.bash.bashrcExtra = {
+    "export SSH_AUTH_SOCK=\"/run/user/1000/yubikey-agent/yubikey-agent.sock\""; # todo remove hard coded 100/0?
   };
 
   programs.ssh = {
