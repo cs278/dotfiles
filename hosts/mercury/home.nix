58f9dbcaa7ee53317bcdced68c4583d947c5b9de
Chris Smith <chris@cs278.org>
2024-08-21 16:53:50 +0100

    [WIP] Test commit. Please ignore

--- hosts/mercury/home.nix
+++ hosts/mercury/home.nix
@@ -92,7 +92,7 @@
   #
   home.sessionVariables = {
     EMAIL = "chris@cs278.org";
-    SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/yubikey-agent/yubikey-agent.sock";
+    SSH_AUTH_SOCK = "/run/user/1000/yubikey-agent/yubikey-agent.sock"; # todo remove hard coded 1000?
     # EDITOR = "emacs";
   };
 
